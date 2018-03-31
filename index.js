6a2563dbcbcc3b8aba0f57b76af4e17285dc8f77
Brass Clarkson <bm.clarkson@gmail.com>
2018-03-31 16:37:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function happyHolidays() {
 function happyHolidaysTo(name) {
   return (`Happy holidays, ${name}!`)
 }
-function happyHolidayTo(`holiday`, `firstName`) {
+function happyHolidayTo(holiday, firstName) {
   return ('Happy ${holiday}, ${firstName}!')
 }
